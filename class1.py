36089ff2fcf306aebc6c58eba0fe42c87fe9ded9
Takzuin <takzuin@gmail.com>
2024-06-06 15:52:31 -0500

    class #1 and #2
    First 2 class in Python Bilingual Course
    (hello world , variables)

---
+++ class1.py
@@ -0,0 +1,10 @@
+# This is a comment in Python. / Este es un comentario en Python.
+# Comments start with the '#' character. / Los comentarios comienzan con el carácter '#'.
+# Comments are useful to explain the code and to make notes for yourself or other programmers.
+# Los comentarios son útiles para explicar el código y para hacer notas para ti mismo o para otros programadores.
+
+# The following line of code prints "Hello, World!" to the console. / La siguiente línea de código imprime "Hello, World!" en la consola.
+print("Hello, World!")
+
+# The 'print' function is a built-in function in Python used to display information on the screen.
+# La función 'print' es una función incorporada en Python que se utiliza para mostrar información en la pantalla.
---
+++ class2.py
@@ -0,0 +1,10 @@
+#Las variables pueden ser ...
+
+#Declaracion de variables 
+
+numero1 = 10       
+numero2 = 20
+numero3 = 100 
+
+#Imprimir valor de las variables
+print(numero1, numero2, numero3)
