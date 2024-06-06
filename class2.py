81d197e50a8a8119256acde7cab49a2bb452420b
Takzuin <takzuin@gmail.com>
2024-06-06 16:10:53 -0500

    Update 6/06/2024
    Class 2 complete. class 3 in process.

--- class2.py
+++ class2.py
@@ -1,10 +1,21 @@
-#Las variables pueden ser ...
+#Las variables pueden ser de diferentes tipos, como enteros, flotantes, cadenas, etc.
+#Variables can be of different types, such as integers, floats, strings, etc.
 
 #Declaracion de variables 
-
-numero1 = 10       
-numero2 = 20
-numero3 = 100 
+#Variable declaration
+numero1 = 10        # Este es un número entero (integer)
+numero2 = 20        # Este es otro número entero (another integer)
+numero3 = 100       # Y este es otro número entero más (and yet another integer)
 
 #Imprimir valor de las variables
-print(numero1, numero2, numero3)
+#Print the value of the variables
+print(numero1, numero2, numero3)  # Esto imprimirá "10 20 100" (This will print "10 20 100")
+
+#Variables de diferentes tipos
+#Variables of different types
+cadena = "Hola, mundo!"  # Esto es una cadena de texto (string)
+flotante = 3.14          # Esto es un número flotante (float)
+
+#Imprimir valor de las variables adicionales
+#Print the value of the additional variables
+print(cadena, flotante)  # Esto imprimirá "Hola, mundo! 3.14" (This will print "Hola, mundo! 3.14")
