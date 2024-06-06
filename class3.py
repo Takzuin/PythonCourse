41f7c50c039c4f02db6a87b22316827cf07f2080
Takzuin <takzuin@gmail.com>
2024-06-06 16:21:30 -0500

    Update class3.py

---
+++ class3.py
@@ -0,0 +1,22 @@
+# Declare three variables a, b, and c in a single line and
+# assign them the values 1, 2, and 3 respectively
+# Declarar tres variables a, b y c en una sola línea y
+# asignarles los valores 1, 2 y 3 respectivamente
+a, b, c = 1, 2, 3
+
+"""
+Just like values can be assigned in the same line,
+they can also be printed in the same line like this:
+print(a, b, c) commas matter
+"""
+"""
+Al igual que se pueden asignar valores en la misma línea, 
+también se pueden imprimir en la misma línea de esta forma:
+print(a, b, c) las comas importan
+"""
+
+# Print the values of a, b, and c
+# Imprimir los valores de a, b y c
+print(a)
+print(b)
+print(c)
